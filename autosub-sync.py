7dd950c9142b9a2a9cc1af3ec300a1e2404b9740
Koenkk <koenkanters94@gmail.com>
2017-06-24 14:27:25 +0200

    Switch to pysrt #1.

--- autosub-sync.py
+++ autosub-sync.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 import argparse
-from datetime import datetime
 from fuzzywuzzy import fuzz
-import unicodedata
 from sklearn import linear_model
 import numpy as np
-import tempfile
 import subprocess
 import sys
-import copy
 import os
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import HoverTool, ColumnDataSource
 from translation import bing
 import progressbar
+import pysrt
 
 
 MATCHER_MAX_TIME_DIFF =  10 * 60 # 10 minutes
@@ -22,138 +19,39 @@ MATCHER_MIN_SCORE = 70
 MOVIE_EXTENSIONS = [".mkv", ".mp4", ".avi"]
 
 
-class SubtitleTrack:
-    def __init__(self, file):
-        self.file = file
-        self.subtitles = []
-
-        with open(file) as srt_file:
-            content = srt_file.read().split('\n')
-
-        def read_content():
-            for line in content:
-                yield line.replace("\r", "")
-
-        content_gen = read_content()
-
-        for line in content_gen:
-            if line is '':
-                break
-
-            lines = [line]
-            while True:
-                l = next(content_gen)
-
-                if l is '' and len(lines) >= 3:
-                    self.subtitles.append(Subtitle(lines))
-                    break
-                else:
-                    lines.append(l)
-
-    def __str__(self):
-        text = ""
-        for subtitle in self.subtitles:
-            text += str(subtitle) + "\n"
-        return text
-
-    def write(self, file_path):
-        subtitles = copy.deepcopy(self.subtitles)
-
-        # Remove subtitles that end on a negative time.
-        subtitles = [s for s in subtitles if s.end > 0]
-
-        # Set time to 0 when subtitle start at negative time.
-        for subtitle in subtitles:
-            if subtitle.start < 0:
-                subtitle.start = 0
-
-        # Reset the ids
-        for i in range (0, len(subtitles)):
-            subtitles[i].ID = i + 1
-
-        # Write subtitle
-        file = open(file_path, "w")
-        for subtitle in subtitles:
-            file.write(subtitle.srt_string())
-            file.write("\n")
-
-        file.close()
+def parse_time_str(sub_time):
+    time = float(sub_time.milliseconds) / 1000
+    time += sub_time.seconds
+    time += sub_time.minutes * 60
+    time += sub_time.hours * 3600
+    return time
 
 
-class Subtitle:
-    def __init__(self, lines):
-        self.ID = lines[0]
-
-        def parse_time(time):
-            time = time.replace(",",".").split(':')
-            time_parsed = int(time[0]) * 3600
-            time_parsed += int(time[1]) * 60
-            time_parsed += float(time[2])
-            return time_parsed
-
-        time = lines[1].split(" --> ")
-        self.start = parse_time(time[0])
-        self.end = parse_time(time[1])
-
-        self.text = []
-        self.text_parsed = ""
-
-        for i in range(2, len(lines)):
-            line = lines[i]
-            self.text.append(line)
-            self.text_parsed += "%s " % line
-
-        # Parse the text to be more fuzzy compare friendly.
-        chars_to_remove = [ "</i>", "<i>", ".", ",", "!", "-", ":", '"', "?"]
-        for char in chars_to_remove:
-            self.text_parsed = self.text_parsed.replace(char, "")
-
-        self.text_parsed = self.text_parsed.lower()
-        self.text_parsed = self.text_parsed.strip()
-
-    def __str__(self):
-        return str({
-            "ID": self.ID,
-            "start": self.start,
-            "end": self.end,
-            "text": self.text,
-            "text_parsed": self.text_parsed
-        })
-
-    def srt_string(self):
-        string = "%s\n" % self.ID
-
-        def seconds_to_string(seconds):
-            m, s = divmod(seconds, 60)
-            h, m = divmod(m, 60)
-            ms = "{:.3f}".format(s).split(".")[1]
-            return "%02d:%02d:%02d,%s" % (h, m, s, ms)
-
-        string += "%s --> %s\n" % (seconds_to_string(self.start), seconds_to_string(self.end))
-
-        for t in self.text:
-            string += "%s\n" % t
-
-        return string
+def remove_punc_from_str(text):
+    chars = [ ".", ",", "!", "-", ":", '"', "?"]
+    for char in chars:
+        text = text.replace(char, "")
+    return text
 
 
 def find_matches(input_track, sync_track):
     matches = []
+    input_track = [(s, parse_time_str(s.start), remove_punc_from_str(s.text_without_tags)) for s in input_track]
+    sync_track = [(s, parse_time_str(s.start), remove_punc_from_str(s.text_without_tags)) for s in sync_track]
 
-    for input_subtitle in input_track.subtitles:
+    for input_subtitle in input_track:
         found_matches = []
 
-        for sync_subtile in sync_track.subtitles:
-            time_diff = abs(sync_subtile.start - input_subtitle.start)
-            if time_diff <= MATCHER_MAX_TIME_DIFF and len(input_subtitle.text_parsed) > MATCHER_MIN_SUBTITLE_LENGTH:
-                score = fuzz.ratio(sync_subtile.text_parsed,
-                                   input_subtitle.text_parsed)
+        for sync_subtitle in sync_track:
+            time_diff = abs(sync_subtitle[1] - input_subtitle[1])
+            if time_diff <= MATCHER_MAX_TIME_DIFF and len(input_subtitle[2]) > MATCHER_MIN_SUBTITLE_LENGTH:
+                score = fuzz.ratio(sync_subtitle[2], input_subtitle[2])
 
                 if score >= MATCHER_MIN_SCORE:
-                    found_matches.append((score, sync_subtile))
+                    found_matches.append((score, sync_subtitle[0]))
 
         if len(found_matches) is 1:
-            matches.append((found_matches[0][0], input_subtitle, found_matches[0][1]))
+            matches.append((found_matches[0][0], input_subtitle[0], found_matches[0][1]))
 
     return matches
 
@@ -171,12 +69,12 @@ def plot_matches(matches, plot_file, coefficient, intercept):
     sync = []
 
     for match in matches:
-        x.append(match[1].start)
-        y.append(match[1].start - match[2].start)
-        input.append(str(match[1]))
-        sync.append(str(match[2]))
-        x_linear_regression.append(match[1].start)
-        y_linear_regression.append((match[1].start * coefficient) + intercept)
+        x.append(parse_time_str(match[1].start))
+        y.append(parse_time_str(match[1].start) - parse_time_str(match[2].start))
+        input.append(match[1].text)
+        sync.append(match[2].text)
+        x_linear_regression.append(parse_time_str(match[1].start))
+        y_linear_regression.append((parse_time_str(match[1].start) * coefficient) + intercept)
 
     source = ColumnDataSource(data=dict(x=x, y=y, input=input, sync=sync))
 
@@ -191,8 +89,8 @@ def calculate_linear_regression(matches):
     y = []
 
     for match in matches:
-        y.append(match[1].start - match[2].start)
-        x.append(match[1].start)
+        y.append(parse_time_str(match[1].start) - parse_time_str(match[2].start))
+        x.append(parse_time_str(match[1].start))
 
     X = np.vander(x, 2)
 
@@ -216,11 +114,13 @@ def calculate_linear_regression(matches):
 
 
 def sync_with_linear_regression(subtitle_track, coefficient, intercept):
-    for subtitle in subtitle_track.subtitles:
-        subtitle.start -= intercept
-        subtitle.end -= intercept
-        subtitle.start -= (subtitle.start * coefficient)
-        subtitle.end -= (subtitle.end * coefficient)
+    import copy
+    subtitle_track = copy.deepcopy(subtitle_track)
+    subtitle_track.shift(seconds=intercept)
+    for subtitle in subtitle_track:
+        subtitle.start -= {'seconds': parse_time_str(subtitle.start) * coefficient}
+        subtitle.end -= {'seconds': parse_time_str(subtitle.end) * coefficient}
+    return subtitle_track
 
 
 def generate_subtitle(video_file, output):
@@ -231,15 +131,24 @@ def generate_subtitle(video_file, output):
     return output
 
 
-def translate_text_parsed(subtitle_track, target_language):
+def translate_subtitle(subtitle_track, target_language):
     print('Translating...')
     bar = progressbar.ProgressBar()
-    for i in bar(range(len(subtitle_track.subtitles))):
+    for subtitle in bar(subtitle_track):
         try:
-            subtitle = subtitle_track.subtitles[i]
-            subtitle.text_parsed = bing(subtitle.text_parsed, dst = 'nl').encode('utf-8','ignore')
+            text = str(remove_punc_from_str(subtitle.text_without_tags))
+            subtitle.text = bing(text, dst = 'nl')
+        except KeyboardInterrupt:
+            sys.exit(1)
         except:
-            print('Failed to translate %s' % (str(subtitle)))
+            print('Failed to translate %s' % (subtitle.text))
+
+
+def open_subtitle(file):
+    try:
+        return pysrt.open(file)
+    except:
+        return pysrt.open(file, encoding='iso-8859-1')
 
 
 def main():
@@ -266,14 +175,14 @@ def main():
             return 1
 
         print("Sync input with subtitle.")
-        input_track = SubtitleTrack(args.input)
-        sync_track = SubtitleTrack(args.sync)
-        translate_text_parsed(sync_track, args.lang)
+        input_track = open_subtitle(args.input)
+        sync_track = open_subtitle(args.sync)
+        translate_subtitle(sync_track, args.lang)
     elif sync_extension in MOVIE_EXTENSIONS:
         print("Sync input with movie.")
-        input_track = SubtitleTrack(args.input)
+        input_track = open_subtitle(args.input)
         generated_subtitle = generate_subtitle(args.sync, args.output)
-        sync_track = SubtitleTrack(generated_subtitle)
+        sync_track = open_subtitle(generated_subtitle)
 
         if not args.keep:
             os.remove(generated_subtitle)
@@ -290,13 +199,23 @@ def main():
         return 1
 
     (coefficient, intercept) = calculate_linear_regression(matches)
+    print(coefficient, intercept)
+
+    input_track = sync_with_linear_regression(input_track, coefficient, intercept)
 
     if args.plot:
         plot_matches(matches, args.plot, coefficient, intercept)
 
-    sync_with_linear_regression(input_track, coefficient, intercept)
+    # Remove subtitles that start and end on 00:00:00.0000.
+    for s in input_track.slice(ends_before={'minutes': 0, 'seconds': 0}):
+        del input_track[input_track.index(s)]
 
-    input_track.write(args.output)
+    # Renumber indexes
+    for s in input_track:
+        s.index = input_track.index(s)
+
+    # Save subtitle.
+    input_track.save(args.output, encoding='utf-8')
 
     print("Wrote automatic synced subtitle file to %s." % args.output)
 
